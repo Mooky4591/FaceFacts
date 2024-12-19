dee5223f06229ce5c3e4bbc3f9f0ca9440192e16
Scott Robinson <SRobinson2@harriscomputer.com>
2024-12-19 15:33:44 -0500

    Initial Commit

---
+++ FaceFacts/FaceFactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceFactsApp.swift
+//  FaceFacts
+//
+//  Created by Michael Robinson on 12/19/24.
+//
+
+import SwiftUI
+
+@main
+struct FaceFactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FaceFactsTests/FaceFactsTests.swift
@@ -0,0 +1,17 @@
+//
+//  FaceFactsTests.swift
+//  FaceFactsTests
+//
+//  Created by Michael Robinson on 12/19/24.
+//
+
+import Testing
+@testable import FaceFacts
+
+struct FaceFactsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
